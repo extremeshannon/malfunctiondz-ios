ef349205082e49d7cfb7d3ec3008a2946021f2b4
Shannon <shannon@extremeshannon.com>
2026-02-26 14:27:30 -0900

    Initial Commit

---
+++ MalfunctionDZ/MalfunctionDZApp.swift
@@ -0,0 +1,17 @@
+//
+//  MalfunctionDZApp.swift
+//  MalfunctionDZ
+//
+//  Created by Shannon on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct MalfunctionDZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
